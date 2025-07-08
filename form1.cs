9cd18a70923a08eb1a4e1b62245b8bf14b81e9c2
eliorharel <mokipolo80@gmail.com>
2025-07-08 22:29:45 +0200

    Create form1.cs

---
+++ form1.cs
@@ -0,0 +1,15 @@
+namespace Zylith
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
