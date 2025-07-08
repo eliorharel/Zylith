dd1d221a95ffe702bfea2fe7fb844617d19d1519
eliorharel <mokipolo80@gmail.com>
2025-07-08 23:35:25 +0200

    Update form1.Designer.cs

--- form1.Designer.cs
+++ form1.Designer.cs
@@ -2,15 +2,8 @@ namespace Zylith
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -19,102 +12,77 @@ namespace Zylith
             }
             base.Dispose(disposing);
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
-            textBox7 = new TextBox();
-            button1 = new Button();
-            listBox1 = new ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBoxTarget = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
             SuspendLayout();
-            // 
-            // textBox1
-            // 
+           
             textBox1.Location = new Point(146, 179);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
+             
             textBox2.Location = new Point(40, 140);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(100, 23);
             textBox2.TabIndex = 1;
-            // 
-            // textBox3
-            // 
+          
             textBox3.Location = new Point(146, 100);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(100, 23);
             textBox3.TabIndex = 2;
-            // 
-            // textBox4
-            // 
+         
             textBox4.Location = new Point(146, 140);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(100, 23);
             textBox4.TabIndex = 3;
-            // 
-            // textBox5
-            // 
+          
             textBox5.Location = new Point(40, 179);
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(100, 23);
             textBox5.TabIndex = 4;
-            // 
-            // textBox6
-            // 
+          
             textBox6.Location = new Point(40, 98);
             textBox6.Name = "textBox6";
             textBox6.Size = new Size(100, 23);
             textBox6.TabIndex = 5;
-            // 
-            // textBox7
-            // 
-            textBox7.Location = new Point(40, 45);
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(206, 23);
-            textBox7.TabIndex = 6;
-            textBox7.TextChanged += textBox7_TextChanged;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(105, 219);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 7;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // listBox1
-            // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(273, 45);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(194, 199);
-            listBox1.TabIndex = 8;
-            // 
-            // Form1
-            // 
+           
+            textBoxTarget.Location = new Point(40, 45);
+            textBoxTarget.Name = "textBox7";
+            textBoxTarget.Size = new Size(206, 23);
+            textBoxTarget.TabIndex = 6;
+            textBoxTarget.TextChanged += textBox7_TextChanged;
+          
+            buttonCalculate.Location = new Point(105, 219);
+            buttonCalculate.Name = "Calculate";
+            buttonCalculate.Size = new Size(75, 23);
+            buttonCalculate.TabIndex = 7;
+            buttonCalculate.Text = "button1";
+            buttonCalculate.UseVisualStyleBackColor = true;
+           
+            listResult.FormattingEnabled = true;
+            listResult.ItemHeight = 15;
+            listResult.Location = new Point(273, 45);
+            listResult.Name = "listBox1";
+            listResult.Size = new Size(194, 199);
+            listResult.TabIndex = 8;
+          
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(listBox1);
-            Controls.Add(button1);
-            Controls.Add(textBox7);
+            Controls.Add(listBoxResult);
+            Controls.Add(buttonCalculate);
+            Controls.Add(textBoxTarget);
             Controls.Add(textBox6);
             Controls.Add(textBox5);
             Controls.Add(textBox4);
@@ -135,8 +103,8 @@ namespace Zylith
         private TextBox textBox4;
         private TextBox textBox5;
         private TextBox textBox6;
-        private TextBox textBox7;
-        private Button button1;
-        private ListBox listBox1;
+        private TextBox textBoxTarget;
+        private Button buttonCalculate;
+        private ListBox listBoxResult;
     }
 }
